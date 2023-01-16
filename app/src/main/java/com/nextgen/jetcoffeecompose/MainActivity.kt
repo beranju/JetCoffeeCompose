1dd51d4fa37f837e055818581c40ac9c8bc55ed1
ribakmasude <beranjusihombing1@gmail.com>
2023-01-16 21:48:10 +0700

    set the banner and category view

--- app/src/main/java/com/nextgen/jetcoffeecompose/MainActivity.kt
+++ app/src/main/java/com/nextgen/jetcoffeecompose/MainActivity.kt
@@ -3,13 +3,24 @@ package com.nextgen.jetcoffeecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.nextgen.jetcoffeecompose.model.dummyCategory
+import com.nextgen.jetcoffeecompose.ui.component.CategoryItem
+import com.nextgen.jetcoffeecompose.ui.component.SearchBar
+import com.nextgen.jetcoffeecompose.ui.component.SectionText
 import com.nextgen.jetcoffeecompose.ui.theme.JetCoffeeComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +28,58 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetCoffeeComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                    JetCoffeeApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun JetCoffeeApp() {
+    Column {
+        Banner()
+        SectionText(title = stringResource(id = R.string.section_category))
+        CategoryRow()
+    }
+    
+}
+
+@Composable
+fun Banner(
+    modifier: Modifier = Modifier
+) {
+    Box(modifier = modifier){
+        Image(
+            painter = painterResource(id = R.drawable.banner),
+            contentDescription = "Banner Image",
+            contentScale = ContentScale.Crop,
+            modifier = modifier.height(160.dp)
+        )
+        SearchBar()
+    }
+}
+
+@Composable
+fun CategoryRow(
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier =  modifier.padding(16.dp)
+    ){
+        items(dummyCategory, key = {it.textCategory}){category->
+            CategoryItem(category = category)
+        }
+    }
+
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetCoffeeComposeTheme {
-        Greeting("Android")
+        JetCoffeeApp()
     }
 }
---
+++ app/src/main/java/com/nextgen/jetcoffeecompose/model/Menu.kt
@@ -0,0 +1,3 @@
+package com.nextgen.jetcoffeecompose.model
+
+
