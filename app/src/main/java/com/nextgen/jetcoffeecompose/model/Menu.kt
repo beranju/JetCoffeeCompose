52a036b75663bc0ad917c7e4c07411664242a2be
ribakmasude <beranjusihombing1@gmail.com>
2023-01-19 22:49:53 +0700

    set the favorite and best menu view

--- app/src/main/java/com/nextgen/jetcoffeecompose/model/Menu.kt
+++ app/src/main/java/com/nextgen/jetcoffeecompose/model/Menu.kt
@@ -1,3 +1,23 @@
 package com.nextgen.jetcoffeecompose.model
 
+import com.nextgen.jetcoffeecompose.R
 
+data class Menu(
+    var name: String,
+    var price: String,
+    var imageUrl: Int
+)
+
+val dummyMenu = listOf<Menu>(
+    Menu("Capucinno", "Rp, 12.000", R.drawable.menu3),
+    Menu("Late", "Rp, 10.000", R.drawable.menu2),
+    Menu("Sanger", "Rp, 15.000", R.drawable.menu4),
+    Menu("Mocacinno", "Rp, 10.000", R.drawable.menu1),
+)
+
+val dummyBestSeller = listOf<Menu>(
+    Menu("Teh Susu Telur", "Rp, 12.000", R.drawable.menu2),
+    Menu("Kopi late Apel", "Rp, 15.000", R.drawable.menu1),
+    Menu("Cappucino Late Dove", "Rp, 15.000", R.drawable.menu3),
+    Menu("Moca black coffee", "Rp, 12.000", R.drawable.menu4),
+)
